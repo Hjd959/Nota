9ac7aa0fd8ab7c6aa62e7c3302b0fb0de6248d40
Abdulwahab <a.hjd959@gmail.com>
2020-07-16 20:11:26 +0300

    add new

---
+++ Nota/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Nota
+//
+//  Created by Abdulwahab Alenezi on 25/11/1441 AH.
+//  Copyright © 1441 Abdulwahab Alenezi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("No thing")
+        print("No thing")
+
+    }
+
+
+}
+
